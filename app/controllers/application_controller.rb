d5fe0958002494784eb1f5a344e9dd4b759fd230
Sarah Freitas <sarahjfreitas@gmail.com>
2019-04-09 14:28:44 -0300

    json response function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
+# classe pai para todos os controllers
 class ApplicationController < ActionController::API
+  # renderiza resposta em json com o status
+  def json_response(object, status = :ok)
+    render json: object, status: status
+  end
 end
